c27705e295ad7d9eb14239ab782ec8f4a2b91eea
zgallup <zgallup@enova.com>
2015-01-12 05:54:27 -0600

    test

--- app.js
+++ app.js
@@ -39,15 +39,13 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+app.get('/', function(req, res) {
+  return res.redirect('http://pacific-eyrie-9417.herokuapp.com');
+});
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Server listening on port', server.address().port);
+});
 
 // production error handler
 // no stacktraces leaked to user
